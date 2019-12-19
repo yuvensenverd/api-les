3a281d74a7bcaaa8267854a96aa60f4296b145c2
mister-ard97 <rezardiansyah1997@gmail.com>
2019-12-19 17:11:56 +0700

    commit

--- controller/adminController.js
+++ controller/adminController.js
@@ -61,9 +61,10 @@ module.exports = {
         })
     },
     adminGetSubscription : (req,res) => {
+        console.log(req.body)
         UserInterest.findAndCountAll({
-            offset:0,
-            limit:10,
+            offset: req.body.offset,
+            limit: req.body.limit,
             attributes : {
                 exclude : ['updatedAt']
             },
@@ -92,6 +93,9 @@ module.exports = {
             console.log(err)
             return res.status(500).send({message : 'ERROR', err})
         })
-    }
+    },
+    // adminDeleteSubscriber = (req, res) => {
+        
+    // }
     
 }
