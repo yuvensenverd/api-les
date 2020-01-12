8f91d36a27b96b94616a6a93c9b320d8639d7da2
mister-ard97 <rezardiansyah1997@gmail.com>
2020-01-12 21:59:54 +0700

    commit

--- migrations/20191126031048-create-location.js
+++ migrations/20191126031048-create-location.js
@@ -34,7 +34,7 @@ module.exports = {
       },
       description : {
         allowNull : false,
-        type : Sequelize.STRING
+        type : Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
--- migrations/20191227031611-add-column-classDate-program.js
+++ migrations/20191227031611-add-column-classDate-program.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Programs', 'classDate', {
-      type: Sequelize.STRING,
+      type: Sequelize.TEXT,
     });
   },
 
