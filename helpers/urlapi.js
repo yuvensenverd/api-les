f50af44431e0eeabd7abe2037e6098f364bbfc7f
mister-ard97 <rezardiansyah1997@gmail.com>
2020-01-27 09:55:58 +0700

    commit

--- helpers/urlapi.js
+++ helpers/urlapi.js
@@ -1,8 +1,9 @@
 // const URL_API = 'http://localhost:2020';
 const URL_API = 'https://api.ngeles.co';
 
-const UI_LINK = 'https://ngeles.co'
+// const UI_LINK = 'https://ngeles.co'
 // const UI_LINK = 'http://localhost:3000'
+const UI_LINK = 'https://sharex.purwadhikax.com'
 
 module.exports = {
     URL_API,
