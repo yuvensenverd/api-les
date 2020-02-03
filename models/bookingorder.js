8f9e1d30f737e6ad42201db6b3792f8972e0c036
mister-ard97 <rezardiansyah1997@gmail.com>
2020-02-03 10:09:31 +0700

    commit

--- models/bookingorder.js
+++ models/bookingorder.js
@@ -1,7 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const BookingOrder = sequelize.define('BookingOrder', {
-    userId: DataTypes.INTEGER
+    userId: DataTypes.INTEGER,
+    programId: DataTypes.INTEGER,
+    venueId: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER,
+    statusPembayaran: DataTypes.STRING
   }, {});
   BookingOrder.associate = function(models) {
     // associations can be defined here
